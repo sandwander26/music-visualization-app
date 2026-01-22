2ba1f899da30f747fd533c9e9cee4921d1318f9d
luvdoomed <luvdoomed@gmail.com>
2026-01-22 11:30:00 +0300

    configure vite paths for tauri

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
-const host = process.env.TAURI_DEV_HOST
+// @ts-expect-error process is a nodejs global
+const host = process.env.TAURI_DEV_HOST;
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  test: {
+    environment: "node",
+    include: ["src/**/*.test.ts"],
+  },
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent Vite from obscuring rust errors
   clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
     host: host || false,
-    hmr: host ? { protocol: 'ws', host, port: 1421 } : undefined,
-    watch: { ignored: ['**/src-tauri/**'] },
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell Vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
   },
-})
+});
