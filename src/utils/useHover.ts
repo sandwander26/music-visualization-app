2cb53b8b151e648c968eb1bc8603e4227c264141
luvdoomed <luvdoomed@gmail.com>
2026-03-25 11:30:00 +0300

    add useHover hook

---
+++ src/utils/useHover.ts
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+export function useHover() {
+  const [hover, setHover] = useState(false)
+  return {
+    hover,
+    bind: {
+      onMouseEnter: () => setHover(true),
+      onMouseLeave: () => setHover(false),
+    },
+  }
+}
