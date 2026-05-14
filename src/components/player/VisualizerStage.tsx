c81218bc4a73b2c0107286e5f7c46b48648d8fba
sand_wander <bogdaartemev@gmail.com>
2026-05-14 11:22:00 +0300

    wrap VisualizerStage with KaraokeLyricsLayer

--- src/components/player/VisualizerStage.tsx
+++ src/components/player/VisualizerStage.tsx
@@ -1,13 +1,18 @@
 import { VisualizerHost } from './VisualizerHost'
+import { KaraokeLyricsLayer } from './KaraokeLyricsLayer'
+import { useUIStore } from '../../store/uiStore'
 
 interface VisualizerStageProps {
   vizId: string
 }
 
 export function VisualizerStage({ vizId }: VisualizerStageProps) {
+  const karaokeOverlay = useUIStore((s) => s.karaokeOverlay)
+
   return (
     <div className="visualizer-stage">
       <VisualizerHost vizId={vizId} />
+      {karaokeOverlay && <KaraokeLyricsLayer variant="overlay" />}
     </div>
   )
 }
